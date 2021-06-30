a726dd7e2a295ee8d03762ca2da6d61944b165ae
Pranavi Daggu <pranavi.daggu@s4-analytics.com>
2021-06-30 16:48:50 +0200

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/te/teProjects/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.te.teProjects.controller.MainView", {});
+});
